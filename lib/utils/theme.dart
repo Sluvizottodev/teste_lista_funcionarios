f8075972c3492ab908cdb9109bc08bce6cfd1cbb
SLuvizotto <luvizottodev@gmail.com>
2025-02-24 22:53:52 -0300

    AppTheme

---
+++ lib/utils/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:teste_pratico_mobile/utils/typography.dart';
+
+final ThemeData appTheme = ThemeData(
+  textTheme: TextTheme(
+    displayLarge: AppTypography.displayLarge,
+    displayMedium: AppTypography.displayMedium,
+    displaySmall: AppTypography.displaySmall,
+  ),
+);
